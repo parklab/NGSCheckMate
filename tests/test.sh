7f823b4a253948d9087da637ec538970dc085caa
soolee <duplexa@gmail.com>
2018-03-07 10:48:28 -0500

    added test for patterngenerator & ngscheckmate_fastq

---
+++ tests/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# patterngenerator test
+./patterngenerator/makesnvpattern.pl tests/SNP_GRCh37_hg19_woChr_21part.bed tests/GRCh37_21part.fa tests/GRCh37_21part tests out
+
+# ngscheckmate_fastq test for the pattern file
+./ngscheckmate_fastq -1 tests/test_21part.fastq tests/out.pt
+
+# remove test pattern files
+rm tests/out*
